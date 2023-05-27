0abd2e1b93bd29e17c5db2e563bd1bceca5e985d
Shubham Singh <shbh29@gmail.com>
2023-05-27 23:11:05 +0530

    compound components integrated.

--- src/compoundComponent/App.js
+++ src/compoundComponent/App.js
@@ -1,30 +1,17 @@
 import Switch from "./Switch/switch";
-import {useState} from 'react';
+import { useState } from "react";
+import './App.css'
 
-function On ({on, children}) {
-    return (
-        on ? <h1>{children}</h1>  : null
-    );
-}
-
-function Off ({on, children}) {
-    return (
-        on ? null : <h1>{children}</h1> 
-    );
-}
-
-
-export default function App () {
-    const [on, setOn] = useState('false');
-    return (
-        <>
-            <h1>
-                Hello Shubham compound component
-            </h1>
-            <Switch onToggle={(on) => setOn(on)}/>
-            <On on={on}>Hi</On>
-            <Off on={on}>Bye</Off>
-            {/* {on ? <h1>Hi</h1> : <h1>Bye</h1>} */}
-        </>
-    );
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello Shubham compound component</h1>
+        <Switch >
+          <Switch.On >Hi</Switch.On>
+          <Switch.Off >Bye</Switch.Off>
+        </Switch>
+      </div>
+    </>
+  );
 }
--- src/compoundComponent/Switch/switch.js
+++ src/compoundComponent/Switch/switch.js
@@ -1,14 +1,17 @@
 import './switch.css'
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-export default function Switch({onToggle}) {
+export default function Switch({onToggle, children}) {
     const [check, setChecked] = useState(false);
     const updateChecked = () => {
         setChecked(!check);
     }
-    useEffect(() => {
-      onToggle(check);
-    }, [check])
+    // useEffect(() => {
+    //   if (onToggle typeof function) {
+    //     onToggle(check);
+    //   }
+    // }, [check])
+
   return (
     <div className="toggle-switch">
         <label className='toggle-label'>
@@ -17,6 +20,24 @@ export default function Switch({onToggle}) {
         <div className="toggle-thumb"></div>
       </div>
       </label>
+
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {on: check});
+      })}
     </div>
   );
 }
+
+Switch.On = ({on, children}) => {
+  return (
+      on ? <h1>{children}</h1>  : null
+  );
+}
+
+Switch.Off = ({on, children}) => {
+  return (
+      on ? null : <h1>{children}</h1>
+  );
+}
+
+
