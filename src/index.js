81fe96901cfb668f529a566f3fceb5d1faac38de
Shubham Singh <shbh29@gmail.com>
2023-05-28 13:46:37 +0530

    React Performance optimization using Should component Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './compoundComponent/App';
+import App from './performanceReact/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/performanceReact/App.js
@@ -0,0 +1,66 @@
+import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+    };
+
+    this._onMessageChange = this._onMessageChange.bind(this);
+    this._getLastMessage = this._getLastMessage.bind(this);
+  }
+  _getLastMessage() {
+    return this.state.messages ? this.state.messages[this.state.messages.length - 1] : "";
+  }
+  _onMessageChange(event) {
+    const {
+      target: { value },
+    } = event;
+    const { messages } = this.state;
+    this.setState({ messages: [...messages, value] });
+  }
+  render() {
+    return (
+      <>
+        <h1> Hello Shubham Performance React</h1>
+        <input type="text" value={this._getLastMessage()} onChange={this._onMessageChange} />
+        <MessageList messages={this.state.messages} />
+      </>
+    );
+  }
+}
+
+class MessageList extends React.Component {
+  render() {
+    // console.log(messages);
+    return (
+      <>
+        <ul>
+          {this.props.messages.map((e) => {
+            return <Message key={e} message={e}></Message>;
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+let count = 0;
+class Message extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    const {message: newProps} = nextProps;
+    const {message: orginalProp} = this.props;
+    return newProps !== orginalProp;
+  }
+  render() {
+    const {message} = this.props;
+    console.log(`update count - ${count++} - ${message}`);
+    return (
+      <li key={message}>{message}</li>
+    )
+  }
+  
+}
